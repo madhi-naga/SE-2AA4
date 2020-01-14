c08f43a01ffa372acbc0c97a120e58c24fd159dd
Madhi <madhi.naga@gmail.com>
2020-01-14 14:21:19 -0500

    Added methods in date_adt.py

--- A1/src/date_adt.py
+++ A1/src/date_adt.py
@@ -10,10 +10,33 @@ class DateT:
   #  @details ?
   #  @param m ...
   def __init__(self, d, m, y):
+    self.d = d
+    self.m = m
+    self.y = y
 
-    
   ## @brief ?
   #  @return ?
   def day(self):
+    return self.d
+
+  ## @brief ?
+  #  @return ?
+  def month(self):
+    return self.m
+
+  ## @brief ?
+  #  @return ?
+  def year(self):
+    return self.y 
+
+  def next(self)
+    (self.d)++
+    return self 
+
+  def prev(self)
+    (self.d)--
+    return self 
+
+
 
   # etc.    
