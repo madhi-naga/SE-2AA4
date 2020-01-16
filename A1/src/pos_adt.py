1924e9122c1d3c22e7b76c7ee2b77c70b0f2f2c7
Madhi <nagarajm@mcmaster.ca>
2020-01-16 18:12:35 -0500

    Added new funcs

--- A1/src/pos_adt.py
+++ A1/src/pos_adt.py
@@ -2,3 +2,45 @@
 #  @author First and Last Name
 #  @brief ?
 #  @date ?
+
+class GPosT:
+
+    #y is latitude, x is longitude
+    def __init__(self, x, y):
+        self.lat = y
+        self.long = x
+
+    def lat(self):
+        return self.lat
+
+    def long(self):
+        return self.long
+
+    def west_of(self, d):
+        if d.long() > self.long():
+            return True
+        else:
+            return False
+
+    def north_of(self, d):
+        if d.lat() < self.lat():
+            return True
+        else:
+            return False
+
+    def equal(self, d):
+        if d.lat() == self.lat() and d.long() == self.long():
+            return True
+        else:
+            return False
+
+    def move(self, b, d):
+        pass
+
+    def distance(self, p):
+        pass
+
+    def arrival_date(self, p, d, s):
+        pass
+
+    
