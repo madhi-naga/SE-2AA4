c3b68135c5cd641410bb2dd370dd6deba9e73622
Madhi <nagarajm@mcmaster.ca>
2020-01-18 23:34:29 -0500

    Updated test cases and fixed leap year bug

--- A1/src/date_adt.py
+++ A1/src/date_adt.py
@@ -13,139 +13,121 @@ class DateT:
     calendar = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
 
     def __init__(self, d, m, y):
-        self.day = d
-        self.month = m
-        self.year = y
+        self.d = d
+        self.m = m
+        self.y = y
 
     ## @brief ?
     def day(self):
-        return self.day
+        return self.d
 
     ## @brief ?
     #  @return ?
     def month(self):
-        return self.month
+        return self.m
 
     ## @brief ?
     #  @return ?
     def year(self):
-        return self.year
+        return self.y
 
     def next(self):
-
         date = DateT(self.day(), self.month(), self.year())
-        """
-        if self.month == 2 and self.day == 28:
-            date.day = 1
-            date.month = date.month + 1
-        if self.day == 30 and (self.month in DateT.M30):
-            date.day = 1
-            date.month = date.month + 1
-        if self.day == 31 and (self.month in DateT.M31):
-            if self.month == 12:
-                date.month = 1
+        if DateT.calendar[date.m] == date.d:
+            if date.m == 12:
                 date.y = date.y + 1
+                date.m = 1
+                date.d = 1
+            elif date.m == 2 and date.y % 4 == 0:
+                if self.y % 100 == 0 and self.y % 400 != 0:
+                    date.m = 3
+                    date.d = 1
+                else:
+                    date.d = 29
             else:
-                date.month = date.month + 1
-            date.day = 1
+                date.m = date.m + 1
+                date.d = 1
+        elif date.m == 2 and date.d == 29:
+            date.m = date.m + 1
+            date.d = 1
         else:
-            date.day = date.day + 1
-        """
-        if DateT.calendar[date.month] == date.day:
-            if date.month == 12:
-                date.year = date.year + 1
-                date.month = 1
-                date.day = 1
-            else:
-                date.month = date.month + 1
-                date.day = 1
-        else:
-            date.day = date.day + 1
+            date.d = date.d + 1
         return date
 
     def prev(self):
-
         date = DateT(self.day(), self.month(), self.year())
-        """
-        if self.day == 1:
-            if self.month - 1 == 2:
-                date.day = 28
-                date.month = date.month - 1
-            if self.day == 31 and (self.month - 1 in DateT.M30):
-                date.day = 30
-                date.month = date.month - 1
-            if self.day == 31 and (self.month - 1 in DateT.M31):
-                date.day = 1
-                date.month = date.month - 1
-        else:
-            date.day = date.day - 1
-        return date
-        """
-        if date.day == 1:
-            if date.month == 1:
-                date.year = date.year - 1
-                date.month = 12
-                date.day = 31
+        if date.d == 1:
+            if date.m == 1:
+                date.y = date.y - 1
+                date.m = 12
+                date.d = 31
+            elif date.m == 3 and date.y % 4 == 0:
+                if (date.y % 100 == 0) and (date.y % 400 != 0):
+                    date.d = 28
+                    date.m = date.m - 1
+                else:
+                    date.d = 29
+                    date.m = date.m - 1
             else:
-                date.day = DateT.calendar[date.month-1]
-                date.month = date.month - 1
+                date.d = DateT.calendar[date.m-1]
+                date.m = date.m - 1
         else:
-            date.day = date.day - 1
+            date.d = date.d - 1
         return date
 
     def before(self, d):
-        if d.year == self.year:
-            if d.month == self.month():
-                if d.day < self.day():
+        if d.y == self.year():
+            if d.m == self.month():
+                if d.d > self.day():
                     return True
                 else:
                     return False
-            elif d.month < self.month():
+            elif d.m > self.month():
                 return True
             else:
                 return False
-        elif d.year < self.year():
+        elif d.y > self.year():
             return True
         else:
             return False
 
     def after(self, d):
-        if d.year == self.year():
-            if d.month == self.month():
-                if d.day > self.day():
+        if d.y == self.year():
+            if d.m == self.month():
+                if d.d < self.day():
                     return True
                 else:
                     return False
-            elif d.month > self.month():
+            elif d.m < self.month():
                 return True
             else:
                 return False
-        elif d.year > self.year():
+        elif d.y < self.year():
             return True
         else:
             return False
 
     def equal(self, d):
-        if d.day == self.day() and d.month == self.month() and d.year == self.year():
+        if d.d == self.day() and d.m == self.month() and d.y == self.year():
             return True
         else:
             return False
 
     def add_days(self, n):
-        d = DateT(self.day, self.month, self.year)
+        d = DateT(self.day(), self.month(), self.year())
         for i in range(n):
-            d.next()
+            d = d.next()
         return d
 
     def days_between(self, d):
         n = 0
-        if self.before(self, d):
-            while not(self.equal(self, d)):
-                d.next()
+        if self.after(d):
+            while not(self.equal(d)):
+                d = d.next()
                 n = n + 1
         else:
-            while not(self.equal(self, d)):
-                d.prev()
+            while not(self.equal(d)):
+                d = d.prev()
                 n = n + 1
         return n
 
--- A1/src/test_driver.py
+++ A1/src/test_driver.py
@@ -3,43 +3,123 @@
 #  @brief ?
 #  @date ?
 
+import unittest
 from date_adt import DateT
 from pos_adt import GPosT
 
+d1 = DateT(1, 1, 2020)
+d11 = DateT(1, 1, 2021)
+d2 = DateT(31, 12, 1500)
+d3 = DateT(25, 4, 1764)
+d4 = DateT(6, 10, 787)
+d44 = DateT(6, 10, 787)
+d5 = DateT(29, 11, 1999)
+d6 = DateT(28, 2, 2220)
+
+p1 = GPosT(43.5, 79.1)
+p2 = GPosT(43.5, 79.1)
+p3 = GPosT(43.5, 79.1)
+p4 = GPosT(43.5, 79.1)
 
 
-def testdates():
-    d1 = DateT(1, 1, 2020)
-    d2 = DateT(31, 12, 1500)
-    d3 = DateT(25, 4, 1764)
-    d4 = DateT(6, 10, 787)
-    d5 = DateT(29, 11, 1999)
-    d6 = DateT(28, 2, 2220)
-    testd()
+class TestT(unittest.TestCase):
 
+    def main(self):
+        self.test_days()
+        self.test_months()
+        self.test_years()
+        self.test_prev()
+        self.test_next()
 
-def testd():
-    assert testdates().d1.day() == 1
-    assert testdates().d2.day() == 31
-    assert testdates().d3.day() == 25
-    assert testdates().d4.day() == 6
-    assert testdates().d5.day() == 29
-    assert testdates().d6.day() == 28
+    def test_days(self):
+        self.assertEqual(d1.day(), 1)
 
+        assert d1.day() == 1
+        assert d2.day() == 31
+        assert d3.day() == 25
+        assert d4.day() == 6
 
-def testm():
-    assert testdates().d1.month() == 1
-    assert testdates().d2.month() == 12
-    assert testdates().d3.month() == 4
-    assert testdates().d4.month() == 10
-    assert testdates().d5.month() == 11
-    assert testdates().d6.month() == 2
+    def test_months(self):
+        assert d1.month() == 1
+        assert d2.month() == 12
+        assert d3.month() == 4
+        assert d4.month() == 10
 
+    def test_years(self):
+        assert d1.year() == 2020
+        assert d2.year() == 1500
+        assert d3.year() == 1764
+        assert d4.year() == 787
 
-def testy():
-    assert testdates().d1.year() == 2020
-    assert testdates().d2.year() == 1500
-    assert testdates().d3.year() == 1764
-    assert testdates().d4.year() == 787
-    assert testdates().d5.year() == 1999
-    assert testdates().d6.year() == 2220
+    def test_next(self):
+        assert d2.next().month() == 1
+        assert d2.next().day() == 1
+        assert d2.next().year() == 1501
+        assert d4.next().month() == 10
+        assert d4.next().day() == 7
+        assert d4.next().year() == 787
+
+    def test_prev(self):
+        assert d1.prev().month() == 12
+        assert d1.prev().day() == 31
+        assert d1.prev().year() == 2019
+        assert d4.prev().month() == 10
+        assert d4.prev().day() == 5
+        assert d4.prev().year() == 787
+
+    def test_after(self):
+        assert d1.after(d2) == True
+        assert d1.after(d3) == True
+        assert d2.after(d3) == False
+        assert d2.after(d4) == True
+
+    def test_before(self):
+        assert d1.before(d2) == False
+        assert d4.before(d1) == True
+        assert d3.before(d1) == True
+        assert d3.before(d4) == False
+
+    def test_equal(self):
+        assert d1.equal(d2) == False
+        assert d4.equal(d44) == True
+
+    def test_add_days(self):
+        assert d2.add_days(7).month() == 1
+        assert d2.add_days(7).day() == 7
+        assert d2.add_days(7).year() == 1501
+        assert d1.add_days(30).month() == 1
+        assert d1.add_days(30).day() == 31
+        assert d1.add_days(30).year() == 2020
+        assert d4.add_days(3650).month() == 10
+        assert d4.add_days(3650).day() == 6
+        assert d4.add_days(3650).year() == 797
+
+    def test_days_between(self):
+        assert d1.days_between(d11) == 365
+
+    def test_lat(self):
+        pass
+
+    def test_long(self):
+        pass
+
+    def test_west_of(self):
+        pass
+
+    def test_north_of(self):
+        pass
+
+    def test_equal(self):
+        pass
+
+    def test_move(self):
+        pass
+
+    def test_distance(self):
+        pass
+
+    def test_arrival_date(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
